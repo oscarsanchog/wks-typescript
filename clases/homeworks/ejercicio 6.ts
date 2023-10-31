3fe58c9a925559d704a309ac7ba5a9f5ddc8fe1b
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-10-30 22:45:30 -0300

    ejercicio 6

--- clases/homeworks/ejercicio 6.ts
+++ clases/homeworks/ejercicio 6.ts
@@ -1,3 +1,5 @@
-function suma(a: number, b: number): number {
-    return a + b
+function suma1(a: number, b: number): string {
+  return a.toString() + b.toString()
 } //que pasa si yo quisiera concatenar strings con esta funcion?
+
+console.log(suma1(2,2));
